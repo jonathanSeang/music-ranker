66f1e75bdee5b74456742350be23f2eac038c2b7
jonathanSeang <70104642+jonathanSeang@users.noreply.github.com>
2023-04-27 11:29:51 -0700

    initial setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,18 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import { invoke } from '@tauri-apps/api/tauri'
+import { useEffect } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
+  useEffect(() => {
+    invoke('greet', { name: 'World' })
+    .then(console.log)
+    .catch(console.error)
+  }, []);
+  
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
